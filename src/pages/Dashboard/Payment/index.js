5661899b43d8806ac03a38d2e0084a1bb653b278
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-05-10 15:42:43 -0300

    feat: warningPage component

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,13 @@
+import WarningPage from '../../../components/WarningPage';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+
 export default function Payment() {
+  const { enrollment } = useEnrollment();
+  const pageTitle = 'Ingresso e pagamento';
+  if (!enrollment) {
+    const warning = 'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso';
+
+    return <WarningPage warning={warning} pageTitle={pageTitle}/>;
+  }
   return 'Pagamento: Em breve!';
 }
