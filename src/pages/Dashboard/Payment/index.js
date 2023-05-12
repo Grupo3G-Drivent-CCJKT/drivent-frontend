51c47e8293a1c09e67b5bd1cf3dd745e83fe9156
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-05-12 08:41:41 -0300

    bug useEffect on dashboard/Payment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,29 +2,28 @@ import WarningPage from '../../../components/WarningPage';
 import useEnrollment from '../../../hooks/api/useEnrollment';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import useCreateTicket from '../../../hooks/api/useCreateTicket';
 import { toast } from 'react-toastify';
+import useToken from '../../../hooks/useToken';
+import * as ticketApi from '../../../services/ticketApi';
 
 export default function Payment() {
   const { enrollment } = useEnrollment();
-  const [ modal, setModal] = useState(undefined);
-  const [ ticket, setTicket] = useState(undefined);
+  const [modal, setModal] = useState(undefined);
+  const [ticket, setTicket] = useState(undefined);
   const { createTicket, createTicketLoading } = useCreateTicket();
-  
+  const [ticketTypes, setTicketType] = useState([]);
+  const [ticketsWithoutHotel, setTicketsWithoutHotel] = useState([]);
+  const token = useToken();
+
   const pageTitle = 'Ingresso e pagamento';
   if (!enrollment) {
     const warning = 'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso';
 
-    return <WarningPage warning={warning} pageTitle={pageTitle}/>;
+    return <WarningPage warning={warning} pageTitle={pageTitle} />;
   }
-
-  let ticketTypes = [{ id: 1, name: 'Online', price: 20000, isRemote: true, includesHotel: false },
-    { id: 2, name: 'Presencial sem hotel', price: 40000, isRemote: false, includesHotel: false },
-    { id: 3, name: 'Presencial com hotel', price: 60000, isRemote: false, includesHotel: true }];
-  
-  let ticketsWithoutHotel = ticketTypes.filter(t => t.includesHotel === false);
 
   function selectModal(event, ticketChoice) {
     event.target.selected = !event.target.selected;
@@ -37,7 +36,7 @@ export default function Payment() {
     if (ticketChoice.isRemote) setTicket(ticketTypes.find(t => t.id === ticketChoice.id));
   }
   function toBRL(value) {
-    return (value/100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return (value / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 
   async function submitTicket() {
@@ -49,22 +48,34 @@ export default function Payment() {
     }
   }
 
+  useEffect(() => {
+    const fetchData = async() => {
+      const data = await ticketApi.getTicketTypes(token);
+      const json = await data.json();
+      setTicketType(json);
+      const ticketsWithoutHotel = ticketTypes.filter(t => t.includesHotel === false);
+      setTicketsWithoutHotel([...ticketsWithoutHotel]);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <StyledTypography variant="h4">{pageTitle}</StyledTypography>
       <StyledTypography variant="h6" color='textSecondary'>Primeiro, escolha sua modalidade de ingresso</StyledTypography>
-      {ticketsWithoutHotel.map(e => <StyledButton selected={modal === e.name} variant="outlined" onClick={(event) => selectModal(event, e)} key={e.id}>{e.name.split(' ')[0]} <br/>
+      {ticketsWithoutHotel.map(e => <StyledButton selected={modal === e.name} variant="outlined" onClick={(event) => selectModal(event, e)} key={e.id}>{e.name.split(' ')[0]} <br />
         {toBRL(e.price)}</StyledButton>)}
       {ticket && <>
 
-        <StyledTypography variant="h6" color='textSecondary'> 
-        Fechado! O total ficou em {toBRL(ticket.price)}. Agora é só confirmar:
+        <StyledTypography variant="h6" color='textSecondary'>
+          Fechado! O total ficou em {toBRL(ticket.price)}. Agora é só confirmar:
         </StyledTypography>
         <Button variant='contained' onClick={submitTicket} disabled={createTicketLoading}>RESERVAR INGRESSO</Button>
       </>}
     </>
   );
-}  
+}
 
 const StyledTypography = styled(Typography)`
   margin: 20px 0 !important;
