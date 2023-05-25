96375dcbfb74cafb38b8fa5ec332069f36688977
kendy-karakawa <kendy.karakawa@gmail.com>
2023-05-24 21:04:30 -0300

    feat: list activities dates

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { datesMock } from './mockActivieties';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
@@ -6,22 +6,42 @@ import { maskDate } from '../../../utils/masks';
 import Locations from './Locations';
 import useTicket from '../../../hooks/api/useTicket';
 import WarningPage from '../../../components/WarningPage';
+import * as activitiesApi from '../../../services/activitiesApi';
+import useToken from '../../../hooks/useToken';
 
 export default function Activities() {
   const [dates, setDates] = useState(datesMock);
   const [dateSelected, setDateSelected] = useState(undefined);
   const { ticket } = useTicket();
+  const token = useToken();
 
   const pageTitle = 'Escolha de atividades';
 
-  if (ticket) {
-    if (ticket.TicketType.isRemote && ticket.status === 'PAID') return <WarningPage
-      warning='Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades.'
-      pageTitle={pageTitle}/>;
+  useEffect(async() => {
+    try {
+      const data = await activitiesApi.getActivitiesDates(token);
+      setDates(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
-    if (ticket.status === 'RESERVED') return <WarningPage
-      pageTitle={pageTitle}
-      warning='Você precisa ter confirmado pagamento antes de fazer a escolha de atividades'/>;
+  if (ticket) {
+    if (ticket.TicketType.isRemote && ticket.status === 'PAID')
+      return (
+        <WarningPage
+          warning="Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades."
+          pageTitle={pageTitle}
+        />
+      );
+
+    if (ticket.status === 'RESERVED')
+      return (
+        <WarningPage
+          pageTitle={pageTitle}
+          warning="Você precisa ter confirmado pagamento antes de fazer a escolha de atividades"
+        />
+      );
   }
 
   function handleSelected(date) {
@@ -39,9 +59,16 @@ export default function Activities() {
   return (
     <>
       <Title variant="h4">Escolha de atividades</Title>
-      <SubTitle variant="h6" selected={dateSelected}>Primeiro, filtre pelo dia do evento:</SubTitle>
+      <SubTitle variant="h6" selected={dateSelected}>
+        Primeiro, filtre pelo dia do evento:
+      </SubTitle>
       <ContainerDates>
-        {dates && dates.map(date => <Date key={date} onClick={() => handleSelected(date)} selected={dateSelected && dateSelected === date}>{maskDate(date)}</Date>)}
+        {dates &&
+          dates.map((date) => (
+            <Date key={date} onClick={() => handleSelected(date)} selected={dateSelected && dateSelected === date}>
+              {maskDate(date)}
+            </Date>
+          ))}
       </ContainerDates>
       <Locations dateSelected={dateSelected} />
     </>
@@ -55,7 +82,7 @@ const Title = styled(Typography)`
 const SubTitle = styled(Typography)`
   margin-left: 0.4rem;
   color: #8e8e8e;
-  display: ${props => props.selected ? 'none' : 'block'};
+  display: ${(props) => (props.selected ? 'none' : 'block')};
 `;
 
 const ContainerDates = styled.div`
@@ -68,7 +95,7 @@ const Date = styled.div`
   width: 130px;
   height: 37px;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
-  background-color: ${props => props.selected ? '#FFD37D' : '#E0E0E0'};
+  background-color: ${(props) => (props.selected ? '#FFD37D' : '#E0E0E0')};
   border-radius: 4px;
   font-family: 'Roboto';
   font-size: 15px;
@@ -78,7 +105,6 @@ const Date = styled.div`
   align-items: center;
   cursor: pointer;
   &:hover {
-    background-color: ${props => props.selected ? '#E0E0E0' : '#FFD37D'};
+    background-color: ${(props) => (props.selected ? '#E0E0E0' : '#FFD37D')};
   }
 `;
-
